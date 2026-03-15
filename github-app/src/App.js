a1b10b307fae3a5bfce7ea3c3e6cf512908e5fef
AlexMooreTech <82064440+AlexMooreTech@users.noreply.github.com>
2026-03-15 15:34:26 +1000

    fixes 2

--- github-app/src/App.js
+++ github-app/src/App.js
@@ -1,13 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  
+  function handleClick() {
+    console.log(position);
+  setPosition({x: Math.random() * 100, y: Math.random() * 100});
+  }
+
+  setPosition(position, {
+    x: position.x += 0.01,
+    y: position.y += 0.1
+  });
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          No, We can have anything here! I now understand this webs stuff!!!
+          No, We can have anything here! I now understand this webs stuff! Testing to see live changes!
         </p>
         <a
           className="App-link"
@@ -15,8 +29,22 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Poorly
         </a>
+            <button
+      style={{
+        position: 'absolute', // Essential for 'top' and 'left' to work
+        left: position.x + 'px',
+        top: position.y + 'px',
+        width: '100px',
+        height: '100px',
+        backgroundColor: 'grey',
+        cursor: 'grab',
+      }}
+      onClick={handleClick}
+    >
+      Click me!
+    </button>
       </header>
     </div>
   );
